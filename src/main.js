f9be598892841b3f9650726c50475e7e7130551d
John Reese <john@noswap.com>
2015-07-15 14:50:16 -0700

    Basic testing of frame/menus/etc

--- src/main.js
+++ src/main.js
@@ -1,20 +1,70 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+var app = require('app')
+var dialog = require('dialog')
+var Menu = require('menu');
+var path = require('path')
+var BrowserWindow = require('browser-window')
+var globalShortcut = require('global-shortcut')
 
-// require('crash-reporter').start();
+// require('crash-reporter').start()
 
-var mainWindow = null;
+var mainWindow = null
+
+var app_root = path.dirname(__dirname)
+var app_path = function(p) {
+    return 'file://' + path.join(app_root, p)
+}
+
+var test = require('./test.js')
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        title: "apollo",
+    })
+    mainWindow.loadUrl(app_path('html/index.html'))
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+
+    console.log(test.foo())
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate([
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'About apollo',
+                    click: function() {
+                        dialog.showMessageBox(mainWindow, {
+                            title: 'About apollo',
+                            message: 'Apollo v' + app.getVersion(),
+                            buttons: ['ok'],
+                        })
+                    }
+                }
+            ]
+        }
+    ]))
+
+    globalShortcut.register('ctrl+n', function() {
+        dialog.showMessageBox(mainWindow, {
+            type: "none",
+            title: "message!",
+            message: "welcome to apollo",
+            buttons: ["ok"],
+        })
+    })
+
+    globalShortcut.register('ctrl+q', function() {
+        app.quit()
+    })
+})
+
+app.on('will-quit', function() {
+    globalShortcut.unregisterAll()
+})
 
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
-        app.quit();
+        app.quit()
     }
-});
-
-app.on('ready', function() {
-    mainWindow = new BrowserWindow({});
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
-});
+})
