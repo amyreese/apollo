9983c85dab1fad421e7bfbcdc1042a6095ced932
John Reese <john@noswap.com>
2015-07-22 13:02:44 -0700

    Better packaging template

--- scripts/package.js
+++ scripts/package.js
@@ -6,37 +6,45 @@ var path = require('path')
 require('./build.js')
 
 var root = path.dirname(__dirname)
-var build_root = path.join(root, 'build')
 var info = JSON.parse(fs.readFileSync(path.join(root, 'package.json')))
+var today = new Date();
+
+var app_name = info.name
+var app_version = info.version
+var app_id = 'com.noswap.apollo'
+var app_icon = 'images/logobig.png'
+var company = 'noswap.com'
+var copyright = 'Copyright ' + today.getFullYear() + ' ' + info.author.name
+
+var binary = process.platform == 'darwin' ? app_name : app_name.toLowerCase()
+var ignore = '(.git|bin|build|makefile|scripts|src|tools)'
 
 function package() {
-  var platform = 'darwin,linux'
-  if (process.platform == 'win32') {
-    platform = 'win32'
-  }
+  var platform = process.platform == 'win32' ? 'win32,linux' : 'darwin,linux'
+  var arch = 'x64'
 
   var options = {
-    'name': 'Apollo',
-    'icon': path.join(root, 'images/logobig.png'),
-    'app-version': info.version,
-    'app-bundle-id': 'com.noswap.apollo',
-    'helper-bundle-id': 'com.noswap.apollo.helper',
+    'name': binary,
+    'icon': path.join(root, app_icon),
+    'app-version': app_version,
+    'app-bundle-id': app_id,
+    'helper-bundle-id': app_id + '.helper',
     'version-string': {
-      'CompanyName': 'noswap.com',
-      'LegalCopyright': 'Copyright 2015 John Reese',
-      'FileDescription': 'Apollo IRC client',
-      'OriginalFilename': 'apollo.exe',
-      'FileVersion': info.version,
-      'ProductVersion': info.version,
-      'ProductName': 'Apollo',
-      'InternalName': 'Apollo',
+      'CompanyName': company,
+      'LegalCopyright': copyright,
+      'FileDescription': app_name,
+      'OriginalFilename': binary + '.exe',
+      'FileVersion': app_version,
+      'ProductVersion': app_version,
+      'ProductName': app_name,
+      'InternalName': app_name,
     },
-    'ignore': '(.git|bin|build|makefile|scripts|src|tools)',
+    'ignore': ignore,
     'dir': root,
-    'out': build_root,
+    'out': path.join(root, 'build'),
     'version': info.devDependencies['electron-prebuilt'],
     'platform': platform,
-    'arch': 'x64',
+    'arch': arch,
     'prune': true,
     'overwrite': true,
   }
