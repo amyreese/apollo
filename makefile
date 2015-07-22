9d1faa788219e37e29a6e3f302ee37645e4a9b09
John Reese <john@noswap.com>
2015-07-22 12:22:33 -0700

    Use electron-packager to build working standalone releases

--- makefile
+++ makefile
@@ -1,14 +1,8 @@
 build: node_modules src/*
-	babel src/ -d js/
-
-watch: node_modules
-	babel --watch src/ -d js/
+	npm run-script build
 
 node_modules: package.json
 	npm install
-
-dev: node_modules
-	npm install -g asar, babel
 
 package: node_modules
 	npm run-script package
@@ -17,4 +11,4 @@ clean:
 	rm -rf build js
 
 distclean:
-	rm -rf build js node_modules tools
+	rm -rf build js node_modules
---
+++ scripts/build.js
@@ -0,0 +1,23 @@
+var babel = require('babel')
+var fs = require('fs')
+var path = require('path')
+var wrench = require('wrench')
+
+var root = path.dirname(__dirname)
+
+function buildjs() {
+  var source = path.join(root, 'src')
+  var target = path.join(root, 'js')
+
+  wrench.mkdirSyncRecursive(target)
+  var files = wrench.readdirSyncRecursive(source)
+  for (var f in files) {
+    var file = files[f]
+    console.log(file)
+
+    var result = babel.transformFileSync(path.join(source, file))
+    fs.writeFileSync(path.join(target, file), result.code)
+  }
+}
+
+buildjs()
